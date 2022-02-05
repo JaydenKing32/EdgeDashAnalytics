94c84c8729fcb59480b077a6e06b4b8504036335
JaydenKing32 <jayden.king@students.mq.edu.au>
2022-02-05 21:04:07 +1100

    Test video list now consists of both inner and outer videos, added comparator to ensure that videos are downloaded in a consistent order, alternating between inner and outer videos

--- app/src/main/java/com/example/edgedashanalytics/util/dashcam/DashCam.java
+++ app/src/main/java/com/example/edgedashanalytics/util/dashcam/DashCam.java
@@ -175,11 +175,10 @@ public class DashCam {
 
     public static Runnable downloadTestVideos(Consumer<Video> downloadCallback, Context context) {
         return () -> {
-            List<String> newVideos = new ArrayList<>(CollectionUtils.disjunction(testVideosBdd, downloads));
-            newVideos.sort(Comparator.comparing(String::toString));
+            List<String> newVideos = new ArrayList<>(CollectionUtils.disjunction(testVideos, downloads));
+            newVideos.sort(Comparator.comparingInt((String a) -> Integer.parseInt(a.substring(4, 6))));
 
             if (newVideos.size() != 0) {
-                // Get oldest new video, testVideos should already be sorted
                 String toDownload = newVideos.get(0);
                 downloadVideo(videoDirUrl + toDownload, downloadCallback, context);
             } else {
@@ -195,73 +194,37 @@ public class DashCam {
     //     return retriever.getFrameAtTime();
     // }
 
-    private static final ArrayList<String> testVideosBdd = new ArrayList<>(Arrays.asList(
-            "b1c66a42-6f7d68ca.mp4",
-            "b1c9c847-3bda4659.mp4",
-            "b1ca2e5d-84cf9134.mp4",
-            "b1cd1e94-26dd524f.mp4",
-            "b1cd1e94-549d0bfe.mp4",
-            "b1ceb32e-a106591d.mp4",
-            "b1d0a191-03dcecc2.mp4",
-            "b1d0a191-06deb55d.mp4",
-            "b1d0a191-2ed2269e.mp4",
-            "b1d0a191-65deaeef.mp4",
-            "b1d0a191-de8948f6.mp4",
-            "b1d10d08-c35503b8.mp4",
-            "b1d22ed6-f1cac061.mp4",
-            "b1d7b3ac-2a92e19f.mp4",
-            "b1dac7f7-6b2e0382.mp4",
-            "b1e0c01d-dd9e6e2f.mp4",
-            "b1e1a7b8-0aec80e8.mp4",
-            "b1e1a7b8-65ec7612.mp4",
-            "b1ee702d-0ae1fc10.mp4",
-            "b1f0efd9-e900c6e5.mp4",
-            "b1f20aa0-3401c3bf.mp4",
-            "b1f4491b-16256d7c.mp4",
-            "b1f4491b-33824f31.mp4",
-            "b1f4491b-9958bd99.mp4",
-            "b1f62c41-ed0c6521.mp4",
-            "b1ff4656-0435391e.mp4",
-            "b2a8e8b4-50058f09.mp4",
-            "b2a8e8b4-a4e93829.mp4",
-            "b2a9d547-f7f6fa92.mp4",
-            "b2ae4fc5-d1082ddf.mp4"
-    ));
-
-
-    private static final ArrayList<String> testVideosPets = new ArrayList<>(Arrays.asList(
-            "S0=City_Center=Time_12-34=View_001.mp4",
-            "S0=City_Center=Time_12-34=View_002.mp4",
-            "S0=City_Center=Time_12-34=View_003.mp4",
-            "S0=City_Center=Time_12-34=View_004.mp4",
-            "S0=City_Center=Time_12-34=View_005.mp4",
-            "S0=City_Center=Time_12-34=View_006.mp4",
-            "S0=City_Center=Time_12-34=View_007.mp4",
-            "S0=City_Center=Time_12-34=View_008.mp4",
-            "S0=City_Center=Time_14-55=View_001.mp4",
-            "S0=City_Center=Time_14-55=View_002.mp4",
-            "S0=City_Center=Time_14-55=View_003.mp4",
-            "S0=City_Center=Time_14-55=View_004.mp4",
-            "S0=Regular_Flow=Time_13-57=View_001.mp4",
-            "S0=Regular_Flow=Time_13-57=View_002.mp4",
-            "S0=Regular_Flow=Time_13-57=View_003.mp4",
-            "S0=Regular_Flow=Time_13-57=View_004.mp4",
-            "S0=Regular_Flow=Time_13-59=View_001.mp4",
-            "S0=Regular_Flow=Time_13-59=View_002.mp4",
-            "S0=Regular_Flow=Time_13-59=View_003.mp4",
-            "S0=Regular_Flow=Time_13-59=View_004.mp4",
-            "S0=Regular_Flow=Time_14-03=View_001.mp4",
-            "S0=Regular_Flow=Time_14-03=View_002.mp4",
-            "S0=Regular_Flow=Time_14-03=View_003.mp4",
-            "S0=Regular_Flow=Time_14-03=View_004.mp4",
-            "S0=Regular_Flow=Time_14-06=View_001.mp4",
-            "S0=Regular_Flow=Time_14-06=View_002.mp4",
-            "S0=Regular_Flow=Time_14-06=View_003.mp4",
-            "S0=Regular_Flow=Time_14-06=View_004.mp4",
-            "S0=Regular_Flow=Time_14-29=View_001.mp4",
-            "S0=Regular_Flow=Time_14-29=View_002.mp4",
-            "S0=Regular_Flow=Time_14-29=View_003.mp4",
-            "S0=Regular_Flow=Time_14-29=View_004.mp4"
+    private static final ArrayList<String> testVideos = new ArrayList<>(Arrays.asList(
+            "out_01.mp4", "inn_01.mp4",
+            "out_02.mp4", "inn_02.mp4",
+            "out_03.mp4", "inn_03.mp4",
+            "out_04.mp4", "inn_04.mp4",
+            "out_05.mp4", "inn_05.mp4",
+            "out_06.mp4", "inn_06.mp4",
+            "out_07.mp4", "inn_07.mp4",
+            "out_08.mp4", "inn_08.mp4",
+            "out_09.mp4", "inn_09.mp4",
+            "out_10.mp4", "inn_10.mp4",
+            "out_11.mp4", "inn_11.mp4",
+            "out_12.mp4", "inn_12.mp4",
+            "out_13.mp4", "inn_13.mp4",
+            "out_14.mp4", "inn_14.mp4",
+            "out_15.mp4", "inn_15.mp4",
+            "out_16.mp4", "inn_16.mp4",
+            "out_17.mp4", "inn_17.mp4",
+            "out_18.mp4", "inn_18.mp4",
+            "out_19.mp4", "inn_19.mp4",
+            "out_20.mp4", "inn_20.mp4",
+            "out_21.mp4", "inn_21.mp4",
+            "out_22.mp4", "inn_22.mp4",
+            "out_23.mp4", "inn_23.mp4",
+            "out_24.mp4", "inn_24.mp4",
+            "out_25.mp4", "inn_25.mp4",
+            "out_26.mp4", "inn_26.mp4",
+            "out_27.mp4", "inn_27.mp4",
+            "out_28.mp4", "inn_28.mp4",
+            "out_29.mp4", "inn_29.mp4",
+            "out_30.mp4", "inn_30.mp4"
     ));
 }
 
