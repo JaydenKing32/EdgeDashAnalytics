5d81ce44434c300481c19cbd713f771b68f485b1
JaydenKing32 <jayden.king@students.mq.edu.au>
2022-03-28 14:43:33 +1100

    format-results.sh now properly handles rows that just contain commas

--- format-results.sh
+++ format-results.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sed 's/,/;,/g' results.csv | column -t -e -s\; | sed 's/ ,/,/g' > temp && mv temp results.csv
+sed 's/,/;,/g' results.csv | sed 's/^;,/!;,/g' | column -t -e -s\; | sed 's/  ,/,/g' | sed 's/^!/ /g' > temp && mv temp results.csv
